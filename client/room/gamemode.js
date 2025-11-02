0b4bbff51dfb76ed8bb364d7d11c2ffbd45a0c22
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-11-02 14:38:22 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -19,7 +19,7 @@ inventory.Main.Value = false, inventory.Secondary.Value = false, inventory.Melee
 Teams.OnRequestJoinTeam.Add(function (p, t) { t.Add(p), p.Spawns.Spawn()});
 // создаем модульные функционированные команды 
  const redTeam = CreateNewTeam('Red', '<b><size=85><color=#962605>尺</color><color=#9a040c>ᴇ</color><color=#b8110b>D</color></size></b>', new Color(125/255, 0, 0, 0), 2, BuildBlocksSet.Red);
- const blueTeam = CreateNewTeam('Blue', '<b><size=85><color=#0d177c>ß</color><color=#03088c>l</color><color=#0607b0>ᴜ</color><color=#1621ae>E</color></size></b>', new Colot(0, 0, 125/255, 0), 1, BuildBlocksSet.Blue);
+ const blueTeam = CreateNewTeam('Blue', '<b><size=85><color=#0d177c>ß</color><color=#03088c>l</color><color=#0607b0>ᴜ</color><color=#1621ae>E</color></size></b>', new Color(0, 0, 125/255, 0), 1, BuildBlocksSet.Blue);
 
  // настройки опции редактора команд
  build.Pipette.Value = true;
